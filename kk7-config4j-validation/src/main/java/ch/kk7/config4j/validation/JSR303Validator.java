5edbe370bc7caa883621043a781bd65ac9dd5ee6
keykey7 <29210485+keykey7@users.noreply.github.com>
2018-10-03 19:34:15 +0200

    getterSelectionStrategy 1 level up

--- kk7-config4j-validation/src/main/java/ch/kk7/config4j/validation/JSR303Validator.java
+++ kk7-config4j-validation/src/main/java/ch/kk7/config4j/validation/JSR303Validator.java
@@ -3,38 +3,23 @@ package ch.kk7.config4j.validation;
 import com.google.auto.service.AutoService;
 import org.hibernate.validator.HibernateValidator;
 import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
-import org.hibernate.validator.spi.properties.ConstrainableExecutable;
-import org.hibernate.validator.spi.properties.GetterPropertySelectionStrategy;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import java.util.Collections;
-import java.util.Optional;
 import java.util.Set;
 
 @AutoService(IValidator.class)
 public class JSR303Validator implements IValidator {
 	private final Validator validator;
 
-	public static class NoPrefixGetterPropertySelectionStrategy implements GetterPropertySelectionStrategy {
-		@Override
-		public Optional<String> getProperty(ConstrainableExecutable executable) {
-			if (executable.getReturnType() == void.class || executable.getParameterTypes().length > 0) {
-				return Optional.empty();
-			}
-			return Optional.of(executable.getName());
-		}
-
-		@Override
-		public Set<String> getGetterMethodNameCandidates(String propertyName) {
-			return Collections.singleton(propertyName);
-		}
+	public JSR303Validator() {
+		validator = newValidator();
 	}
 
-	public JSR303Validator() {
-		validator = Validation.byProvider(HibernateValidator.class)
+	protected Validator newValidator() {
+		return Validation.byProvider(HibernateValidator.class)
 				.configure()
 				.getterPropertySelectionStrategy(new NoPrefixGetterPropertySelectionStrategy())
 				.messageInterpolator(new ParameterMessageInterpolator())
