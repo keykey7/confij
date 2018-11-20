93462491e4ea23b53e184c50d3e06073cc94be20
keykey7 <29210485+keykey7@users.noreply.github.com>
2018-11-20 11:25:30 +0100

    hibernate-validation doc

---
+++ confij-documentation/src/test/java/ch/kk7/confij/docs/Validation.java
@@ -0,0 +1,27 @@
+package ch.kk7.confij.docs;
+
+import ch.kk7.confij.pipeline.ConfijBuilder;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+public class Validation implements WithAssertions {
+
+	// tag::jsr303-interface[]
+	interface Validated {
+		@NotNull
+		String mandatory();
+		@Pattern(regexp = "[A-Z]*")
+		String uppercase();
+	}
+	// end::jsr303-interface[]
+
+	@Test
+	public void isValidated() {
+		assertThatThrownBy(() -> ConfijBuilder.of(Validated.class)
+				.build()).isInstanceOf(ConstraintViolationException.class);
+	}
+}
